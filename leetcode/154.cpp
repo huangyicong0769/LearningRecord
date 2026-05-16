e148e6da500efa72084b52f99cae17b2532213e8
huangyicong0769 <mcrspsht@outlook.com>
2026-05-16 14:44:21 +0800

    lc 154

---
+++ leetcode/154.cpp
@@ -0,0 +1,15 @@
+#include "lc.hpp"
+
+class Solution1 {
+public:
+    int findMin(vector<int>& nums) {
+        return std::ranges::min(nums);
+    }
+};
+
+class Solution2 {
+public:
+    int findMin(vector<int>& nums) {
+
+    }
+};

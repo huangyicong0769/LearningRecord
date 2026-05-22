7adeaa8374a4405642ca861f38c30b26374eb67c
huangyicong0769 <mcrspsht@outlook.com>
2026-05-22 09:12:08 +0800

    lc 2540

---
+++ leetcode/2540.cpp
@@ -0,0 +1,14 @@
+#include "lc.hpp"
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        for (int i = 0, j = 0; i < nums1.size() && j < nums2.size(); ){
+            if (nums1[i] == nums2[j])
+                return nums1[i];
+            while (i < nums1.size() && nums1[i] < nums2[j]) i++;
+            while (j < nums2.size() && nums1[i] > nums2[j]) j++;
+        }
+        return -1;
+    }
+};

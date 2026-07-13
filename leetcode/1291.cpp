919a43a374b7f4d5e26b88a263ecb657d76570c8
huangyicong0769 <mcrspsht@outlook.com>
2026-07-13 08:41:50 +0800

    lc 1291

---
+++ leetcode/1291.cpp
@@ -0,0 +1,24 @@
+#include "lc.hpp"
+
+class Solution {
+public:
+    constexpr static std::array<int, 36> seqDigits = {
+        12, 23, 34, 45, 56, 67, 78, 89,
+        123, 234, 345, 456, 567, 678, 789,
+        1234, 2345, 3456, 4567, 5678, 6789,
+        12345, 23456, 34567, 45678, 56789,
+        123456, 234567, 345678, 456789,
+        1234567, 2345678, 3456789,
+        12345678, 23456789,
+        123456789
+    };
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int> result;
+        for (int num : seqDigits) {
+            if (num >= low && num <= high) {
+                result.push_back(num);
+            }
+        }
+        return result;
+    }
+};
